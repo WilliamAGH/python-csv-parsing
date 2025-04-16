82cb02226f5bcf1d4a1754a58d8a47c628d3df3e
Vasilisa Zaitseva <vasilisa.zaitseva@gmail.com>
2025-04-15 18:21:27 -0700

    definition of the class "Country" and functions for grouping countries by regions

--- lab_3.py
+++ lab_3.py
@@ -1,4 +1,9 @@
+# CIS-117 Lab3
+# Reads the CSV file containing information on countries and splits the countries by region generating one file per region
+# Group# 3 William Callahan - reading part, Vasilisa Zaitseva - writting part
+
 import os
+from typing import List, Dict
 
 class Country:
     def __init__(self, name: str, region: str):
@@ -12,12 +17,17 @@ def group_by_region(countries: list[Country]) -> Dict[str, List[Country]]:
     return region_dict
 
 def write_to_file(countries: list[Country], file_name: str):
-    pass
-
+    try:
+        with open(file_name, 'w') as f:
+            f.write("Name,Region\n")
+            for country in countries:
+                f.write(f"{country.name},{country.region}\n")
+    except (IOError, FileNotFoundError, PermissionError) as e:
+        print(f"Error writing to {file_name}: {e}")
 
 def split_by_region(countries: list[Country], directory: str):
     regions = group_by_region(countries)
-    os.makedirs("regions", exist_ok=True)
-    for key, value in regions.items:
+    os.makedirs(directory, exist_ok=True)
+    for key, value in regions.items():
         file_path = os.path.join(directory, f"{key}.csv")
         write_to_file(value, file_path)
